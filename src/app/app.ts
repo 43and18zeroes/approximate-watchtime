d6a577f4ce068625b0c0fd457ea03267f76d890b
Christoph <c.wagner.germany@gmail.com>
2025-08-01 16:32:57 +0200

    dmservice

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
-import { Component, computed, effect, signal } from '@angular/core';
+import { Component, computed, effect, inject, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { CustomSidenav } from "./components/custom-sidenav/custom-sidenav";
+import { CustomSidenav } from './components/custom-sidenav/custom-sidenav';
+import { ThemeService } from './services/theme-service';
 
 @Component({
   selector: 'app-root',
@@ -15,46 +16,19 @@ import { CustomSidenav } from "./components/custom-sidenav/custom-sidenav";
     MatSidenavModule,
     MatButtonModule,
     MatSlideToggleModule,
-    CustomSidenav
-],
+    CustomSidenav,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('crud');
-  darkMode = signal(false);
+  themeService = inject(ThemeService);
   collapsed = signal(false);
 
   sidenavWidth = computed(() => (this.collapsed() ? '65px' : '250px'));
 
-  ngOnInit() {
-    this.loadDarkModeFromStorageOrSystem();
-    this.listenToSystemPreferenceChanges();
+  constructor() {
+    this.themeService.initTheme();
   }
-
-  private loadDarkModeFromStorageOrSystem(): void {
-    const saved = localStorage.getItem('darkMode');
-    if (saved !== null) {
-      this.darkMode.set(saved === 'true');
-    } else {
-      const prefersDark = window.matchMedia?.('(prefers-color-scheme: dark)').matches;
-      this.darkMode.set(prefersDark);
-    }
-  }
-
-  private listenToSystemPreferenceChanges(): void {
-    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
-      const saved = localStorage.getItem('darkMode');
-      if (saved === null) {
-        this.darkMode.set(e.matches);
-      }
-    });
-  }
-
-  toggleDarkMode = effect(() => {
-    const dark = this.darkMode();
-    document.body.classList.toggle('darkMode', dark);
-    document.body.classList.toggle('lightMode', !dark);
-    localStorage.setItem('darkMode', dark.toString());
-  });
 }
