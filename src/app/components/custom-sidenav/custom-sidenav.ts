405585d79bc11d37e1bf41f7af506855b343fbd7
Christoph <c.wagner.germany@gmail.com>
2025-08-03 14:15:56 +0200

    list

--- src/app/components/custom-sidenav/custom-sidenav.ts
+++ src/app/components/custom-sidenav/custom-sidenav.ts
@@ -1,8 +1,17 @@
 import { Component, Input, signal } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { RouterModule } from '@angular/router';
+
+export type MenuItem = {
+  icon: string;
+  label: string;
+  route: string;
+};
 
 @Component({
   selector: 'app-custom-sidenav',
-  imports: [],
+  imports: [MatListModule, MatIconModule, RouterModule],
   templateUrl: './custom-sidenav.html',
   styleUrl: './custom-sidenav.scss',
 })
@@ -12,4 +21,12 @@ export class CustomSidenav {
   @Input() set collapsed(val: boolean) {
     this.sideNavCollapsed.set(val);
   }
+
+  menuItems = signal<MenuItem[]>([
+    {
+      icon: 'forms_add_on',
+      label: 'Data Entry',
+      route: '/data-entry',
+    },
+  ]);
 }
