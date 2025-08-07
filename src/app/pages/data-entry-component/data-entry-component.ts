81a6b9f3a23a6ae7f1d35b8bbbfa5254087bcffd
Christoph <c.wagner.germany@gmail.com>
2025-08-07 16:52:24 +0200

    submit

--- src/app/pages/data-entry-component/data-entry-component.ts
+++ src/app/pages/data-entry-component/data-entry-component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
@@ -6,10 +7,16 @@ import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-data-entry-component',
-  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [CommonModule, FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './data-entry-component.html',
-  styleUrl: './data-entry-component.scss'
+  styleUrl: './data-entry-component.scss',
 })
 export class DataEntryComponent {
+  minutes: string = '';
+  seconds: string = '';
+  submitted: boolean = false;
 
+  submit() {
+    this.submitted = true;
+  }
 }
