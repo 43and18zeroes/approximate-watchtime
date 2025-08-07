c28b35bd19009559cbb6655e9a10939304be8238
Christoph <c.wagner.germany@gmail.com>
2025-08-07 16:44:46 +0200

    btn

--- src/app/pages/data-entry-component/data-entry-component.ts
+++ src/app/pages/data-entry-component/data-entry-component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-data-entry-component',
-  imports: [FormsModule, MatFormFieldModule, MatInputModule],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './data-entry-component.html',
   styleUrl: './data-entry-component.scss'
 })
