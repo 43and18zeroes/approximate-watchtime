d351cd1d6a1debb8afccfd43fa80ec3eb20de62d
Christoph <c.wagner.germany@gmail.com>
2025-08-24 16:37:39 +0200

    disclaimer

--- src/app/components/data-analysis-component/data-analysis-component.ts
+++ src/app/components/data-analysis-component/data-analysis-component.ts
@@ -2,12 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component, Inject } from '@angular/core';
 import { MatBottomSheetModule, MatBottomSheetRef, MAT_BOTTOM_SHEET_DATA } from '@angular/material/bottom-sheet';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 export type AnalysisData = { formatted: string; seconds: number };
 
 @Component({
   selector: 'app-data-analysis-component',
-  imports: [CommonModule, MatBottomSheetModule, MatButtonModule],
+  imports: [CommonModule, MatBottomSheetModule, MatButtonModule, MatIconModule],
   templateUrl: './data-analysis-component.html',
   styleUrl: './data-analysis-component.scss',
 })
